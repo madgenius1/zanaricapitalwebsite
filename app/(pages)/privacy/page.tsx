24023fc25b748099116a92fc3d3a68aacc65f226
madgenius1 <nickjuma@outlook.com>
2026-01-28 03:25:15 +0300

    Modified /team/page.tsx and added app/(pages)/contact/         app/(pages)/privacy/         app/(pages)/terms/

---
+++ app/(pages)/privacy/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Page() {
+    return(
+        <main className="bg-gray-50 dark:bg-gray-800">
+            <div className="min-h-screen flex justify-center py-12 px-12 items-center">
+                <h1 className="text-gray-950 dark:text-gray-50 text-md">
+                    Privacy Policy Page
+                </h1>
+            </div>
+        </main>
+    )
+}
